179e7b0b5089883265d252232c7a6cd17efe02c3
Rustan Corpuz <rustanacexd@gmail.com>
2014-09-02 00:11:12 +0800

    Preparing for github mac

--- content-optin.php
+++ content-optin.php
@@ -1,17 +1,17 @@
 <?php 
 
-
 for ($i=1 ; $i <= 4; $i++) { 
-   if (!empty(get_post_meta($post->ID,'leadbox_code'.$i,true ))) { ?>
+  $code = get_post_meta($post->ID,'leadbox_code'.$i,true );
+   if (!empty($code)) { ?>
  <div style="background-color: #eef1f5;margin-top:40px">
     <section class="container">
         <div id="optin" class="row clearfix" style="position: relative">
                   <div class="optin-smiley">
                   <img class="text-center hidden-xs" style="position: absolute;left:47%;bottom:85%"src="<?php echo get_template_directory_uri() .'/images/optin-smiley.png' ?>" alt="smiley">
-                 <h2 class="text-center"><?php the_field('optin_text'); ?></h2>
+             <h2 class="text-center"><?php echo get_post_meta($post->ID,'leadbox_headline'.$i,true ) ;?></h2> 
                 
                 </div>
-               <div class="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3"> 
+               <div class="col-xs-8 col-xs-offset-2 smiley"> 
 
                <?php echo get_post_meta($post->ID,'leadbox_code'.$i,true ) ;?>
 
